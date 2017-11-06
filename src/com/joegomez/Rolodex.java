6d0289b2397d5bbd95c41d11086d9c53c58ce88c
joeandrewgomez <joeandrewgomez@gmail.com>
2017-11-06 09:55:56 -0500

    Passing methods and constructors to ArrayLists

--- src/com/joegomez/Rolodex.java
+++ src/com/joegomez/Rolodex.java
@@ -20,4 +20,19 @@ public class Rolodex {
         myContacts.add(contact);
         return true;
     }
+    private int findContact(Contact contact){
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName){
+        for(int i=0; i<this.myContacts.size(); i++){
+            Contact contact = this.myContacts.get(i);
+            if(contact.getName().equals(contactName)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
