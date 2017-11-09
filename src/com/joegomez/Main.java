033078d16cb8862a0fbdf6d03303e6f04bf48bde
joeandrewgomez <joeandrewgomez@gmail.com>
2017-11-09 16:01:28 -0500

    Trying to finish this out.

--- src/com/joegomez/Main.java
+++ src/com/joegomez/Main.java
@@ -1,8 +1,52 @@
 package com.joegomez;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Rolodex rolodex = new Rolodex("0039 330 4484");
 
     public static void main(String[] args) {
-        System.out.println("Boom");
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while(!quit){
+            System.out.println("\nEnter actions: (6 to show available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("\nShutting down....");
+                    quit = true;
+                    break;
+                case 1:
+                    printContacts();
+                    break;
+            }
+
+        }
+    }
+
+    private static void printContacts(){
+        rolodex.printContacts();
+
+
+    }
+
+    private static void startPhone(){
+        System.out.println("Starting a phone....");
+    }
+
+    private static void printActions(){
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println("0 - to shutdown\n" +
+                           "1 - to print contacts\n" +
+                           "2 - to add a new contact\n" +
+                           "3 - to update existing contact\n"+
+                           "4 - to remove an existing contact \n" +
+                           "5 - query if an existing contact exists\n" +
+                           "6 - to print a lust of available actions.");
+        System.out.println("Choose your actions: ");
     }
 }
--- src/com/joegomez/Rolodex.java
+++ src/com/joegomez/Rolodex.java
@@ -30,6 +30,16 @@ public class Rolodex {
         System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
         return true;
     }
+    public boolean removeContact(Contact contact){
+        int foundPosition = findContact(contact);
+        if(foundPosition <0){
+            System.out.println(contact.getName() +", was not found. ");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() +", was deleted.  ");
+    }
+
     private int findContact(Contact contact){
         return this.myContacts.indexOf(contact);
     }
@@ -42,6 +52,22 @@ public class Rolodex {
             }
         }
         return -1;
+    }
+
+    public String queryContact(Contact contact){
+        if(findContact(contact)>=0) {
+            return contact.getName();
+        }
+        return null;
+
+    }
+    public void printContacts(){
+        System.out.println("Contact List");
+        for(int i=0; i<this.myContacts.size(); i++){
+            System.out.println((i+1) + "." +
+                        this.myContacts.get(i).getName() + " ->" +
+                        this.myContacts.get(i).getPhoneNumber());
+        }
     }
 
 
